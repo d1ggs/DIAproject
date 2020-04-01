1d2a90c75e77c4b912137c80c35ad1cd7cd87ad9
d1ggs <diego.piccinotti@mail.polimi.it>
2020-04-01 16:02:40 +0200

    SW-UCB# implementation

--- prova_ucb/UCBLearner.py
+++ prova_ucb/UCBLearner.py
@@ -18,4 +18,41 @@ class UCBLearner(Learner):
         self.upper_bounds[pulled_arm] = x + np.sqrt((2 * np.log(self.t)) / len(self.rewards_per_arm[pulled_arm]))
 
 
+class SWUCBLearner(UCBLearner):
+    """Class implementing SW-UCB# state-of-the-art bandit for non-stationary contexts"""
+    def __init__(self, n_arms: int, lamda: float, alpha: float):
+        assert lamda >= 0, "lambda must be equal or greater than 0"
+        assert 1 >= alpha > 0, 'alpha must satisfy 0 < alpha <= 1'
+        super().__init__(n_arms)
+        self.lamda = lamda
+        self.alpha = alpha
+        self.pulls = []
 
+    def get_tau(self):
+        return min(self.t, np.ceil(self.lamda * (self.t ** self.alpha)))
+
+    def get_times_pulled(self, arm):
+        start = self.t - self.get_tau() + 1
+        return self.pulls[start:].count(arm)
+
+    def update_observations(self, pulled_arm: int, reward: float):
+        # Store the reward, putting 0s to those arms that were not pulled
+        for i in range(len(self.rewards_per_arm)):
+            self.rewards_per_arm[i].append(reward if i == pulled_arm else 0)
+        self.collected_rewards = np.append(self.collected_rewards, reward)
+
+    def update(self, pulled_arm, reward):
+        self.pulls.append(pulled_arm)
+        self.update_observations(pulled_arm, reward)
+        self.t += 1
+
+        # Compute the empirical mean only for those pulls that are in the window
+
+        start = self.t - self.get_tau() + 1
+        r = 1/self.get_times_pulled(pulled_arm) * np.sum(self.rewards_per_arm[pulled_arm][start:])
+
+        # Compute the uncertainty bound for the window
+        c = np.sqrt(((1+self.alpha) * np.log(self.t))/self.get_times_pulled(pulled_arm))
+
+        # Update the overall bound
+        self.upper_bounds[pulled_arm] = r + c
