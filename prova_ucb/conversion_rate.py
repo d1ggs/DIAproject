6f1cad99e467f0e388d97fa866fc7cadc73ab2d4
LorenzoCasalini <lollocasalini@gmail.com>
2020-04-02 09:49:04 +0200

    New conversion rates

--- prova_ucb/conversion_rate.py
+++ prova_ucb/conversion_rate.py
@@ -57,6 +57,63 @@ class Product1Season1(ConversionRateCurve):
         plt.show()
 
 
+class Product1Season2(ConversionRateCurve):
+    def __init__(self):
+        self.points = np.array(
+            [[0, 1], [0.5, 0.99], [1, .95], [1.5, .9], [2, .85], [2.5, .8], [3, .77], [3.5, .65], [4, .45], [4.5, .30],
+             [5, .1], [5.5, 0], [6, 0], [6.5, 0], [7, 0]])
+        self.x = self.points[:, 0]
+        self.y = self.points[:, 1]
+        self.z = np.polyfit(self.x, self.y, 6)
+        self.p = np.poly1d(self.z)
+
+    def get_probability(self, price: int):
+        prob = self.p(price)
+        if price < 0 or price > 4:
+            return 0
+        elif prob > 1:
+            return 1
+        elif prob < 0:
+            return 0
+        else:
+            return prob
+
+    def plot(self):
+        new_x = np.linspace(0, 6, 100)
+        plt.figure("Product1 season2")
+        plt.plot(new_x, self.p(new_x), 'r')
+        plt.show()
+
+
+class Product1Season2(ConversionRateCurve):
+    def __init__(self):
+        self.points = np.array(
+            [[0, 1], [0.5, 0.9], [1, .85], [1.5, .75], [2, .60], [2.5, .40], [3, .20], [3.5, .10], [4, .05], [4.5, .0],
+             [5, 0], [5.5, 0], [6, 0], [6.5, 0], [7, 0]])
+        self.x = self.points[:, 0]
+        self.y = self.points[:, 1]
+        self.z = np.polyfit(self.x, self.y, 6)
+        self.p = np.poly1d(self.z)
+
+    def get_probability(self, price: int):
+        prob = self.p(price)
+        if price < 0 or price > 6:
+            return 0
+        elif prob > 1:
+            return 1
+        elif prob < 0:
+            return 0
+        else:
+            return prob
+
+    def plot(self):
+        new_x = np.linspace(0, 7, 100)
+        plt.figure("Product1 season2")
+        plt.plot(new_x, self.p(new_x), 'r')
+        plt.show()
+
+
+
 class DemandModel(object):
     def __init__(self, cr_curve, n_arms):
         self.cr_curve = cr_curve
