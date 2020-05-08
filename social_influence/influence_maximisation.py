3ab87c848ff8d6a9bb0ad947c5d3d7d6ea33d8a1
d1ggs <diego.piccinotti@mail.polimi.it>
2020-05-08 15:19:20 +0200

    Refactor

--- social_influence/influence_maximisation.py
+++ social_influence/influence_maximisation.py
@@ -6,6 +6,7 @@ from abc import ABC, abstractmethod
 
 from social_influence.mc_sampling import MonteCarloSampling
 
+
 class SingleInfluenceLearner(ABC):
     """
     Abstract Class that represent a Influence Maximisation learner for a single Social Network
@@ -17,9 +18,11 @@ class SingleInfluenceLearner(ABC):
     n_nodes : number of nodes in the graph
 
     """
-    def __init__(self, prob_matrix, n_nodes : int):
+
+    def __init__(self, prob_matrix, n_nodes: int):
         self.prob_matrix = prob_matrix
         self.n_nodes = n_nodes
+
     @abstractmethod
     def fit(self):
         pass
@@ -27,17 +30,17 @@ class SingleInfluenceLearner(ABC):
 
 class GreedyLearner(SingleInfluenceLearner):
 
-    def pool_worker(self, node: int,best_seeds: np.ndarray, mc_sim : int, n_steps: int):
+    def pool_worker(self, node: int, best_seeds: np.ndarray, mc_sim: int, n_steps: int):
         sampler = MonteCarloSampling(self.prob_matrix)
         seeds = np.copy(best_seeds)
-        seeds[node] = 1 #add current node to seeds
+        seeds[node] = 1  # add current node to seeds
 
         nodes_probabilities = sampler.mc_sampling(seeds, mc_sim, n_steps)
         influence = np.sum(nodes_probabilities)
 
-        return node,influence
+        return node, influence
 
-    def parallel_fit(self, budget: int, montecarlo_simulations : int, n_steps_max: int):
+    def parallel_fit(self, budget: int, montecarlo_simulations: int, n_steps_max: int):
 
         max_influence = 0
         best_seeds = np.zeros(self.n_nodes)
@@ -47,31 +50,33 @@ class GreedyLearner(SingleInfluenceLearner):
             step_influence = max_influence
 
             results_async = []
-            results=[]
+            results = []
             with Pool() as pool:
                 for n in range(self.n_nodes):
-                    r_async = pool.apply_async(self.pool_worker, args=(n, best_seeds, montecarlo_simulations, n_steps_max))
+                    r_async = pool.apply_async(self.pool_worker,
+                                               args=(n, best_seeds, montecarlo_simulations, n_steps_max))
                     results_async.append(r_async)
 
                 for r in results_async:
                     results.append(r.get())
 
+                pool.close()
+
             for res in results:
-                n,influence = res     
+                n, influence = res
                 marginal_increase = influence - step_influence
 
                 if marginal_increase > best_marginal_increase:
                     best_node = n
                     best_marginal_increase = marginal_increase
                     max_influence = influence
-            
+
             best_seeds[best_node] = 1
-            print("Node with best marginal increase at step %d: %d" % (i+1, best_node))
+            print("Node with best marginal increase at step %d: %d" % (i + 1, best_node))
 
         return best_seeds, max_influence
-        
 
-    def fit(self, budget: int, montecarlo_simulations : int, n_steps_max: int):
+    def fit(self, budget: int, montecarlo_simulations: int, n_steps_max: int):
         """
         Greedy influence maximization algorithm
         
@@ -94,11 +99,11 @@ class GreedyLearner(SingleInfluenceLearner):
 
             for n in range(self.n_nodes):
                 if best_seeds[n] == 0:
-                    #computer marginal increase
+                    # computer marginal increase
                     seeds = np.copy(best_seeds)
-                    seeds[n] = 1 #add current node to seeds
+                    seeds[n] = 1  # add current node to seeds
 
-                    #computer marginal increase
+                    # computer marginal increase
                     nodes_probabilities = sampler.mc_sampling(seeds, montecarlo_simulations, n_steps_max)
                     influence = np.sum(nodes_probabilities)
 
@@ -108,21 +113,21 @@ class GreedyLearner(SingleInfluenceLearner):
                         best_node = n
                         best_marginal_increase = marginal_increase
                         max_influence = influence
-                if n%100==0:
+                if n % 100 == 0:
                     print("Analysing node: %d of %d" % (n, self.n_nodes))
-            
+
             best_seeds[best_node] = 1
-            print("Node with best marginal increase at step %d: %d" % (i+1, best_node))
+            print("Node with best marginal increase at step %d: %d" % (i + 1, best_node))
 
         return best_seeds, max_influence
 
-        
+
 class ExactSolutionLearner(SingleInfluenceLearner):
     """
     Exact Solution Influence Maximisation
     """
-    
-    def fit(self, budget: int, montecarlo_simulations : int, n_steps_max: int):
+
+    def fit(self, budget: int, montecarlo_simulations: int, n_steps_max: int):
         """
         Basic exact influence maximization algorithm which enumerates all seeds node given a budget. Returns indexes of best seeds 
         
@@ -134,34 +139,29 @@ class ExactSolutionLearner(SingleInfluenceLearner):
 
         n_steps_max : max number of steps in a simulation
         """
-        
 
         sampler = MonteCarloSampling(self.prob_matrix)
 
-        nodes = [d for d in range(0,self.n_nodes)]
-        #seeds_combinations = list(combinations(nodes, self.budget)) #this take too much memory
-        
+        nodes = [d for d in range(0, self.n_nodes)]
+        # seeds_combinations = list(combinations(nodes, self.budget)) #this take too much memory
 
         max_influence = 0
         best_seeds = np.zeros(self.n_nodes)
-        
 
-        for combination in combinations(nodes, budget): #enumerate all possible seeds given a budget
+        for combination in combinations(nodes, budget):  # enumerate all possible seeds given a budget
             seeds = np.zeros(self.n_nodes)
-            #print("Current Seeds: [%s]" % (','.join(str(n) for n in combination)))
+            # print("Current Seeds: [%s]" % (','.join(str(n) for n in combination)))
             seeds[list(combination)] = 1
-            
+
             nodes_probabilities = sampler.mc_sampling(seeds, montecarlo_simulations, n_steps_max)
 
-            #the best seed is the one where the sum of probabilities is the highest
-            influence = np.sum(nodes_probabilities) 
+            # the best seed is the one where the sum of probabilities is the highest
+            influence = np.sum(nodes_probabilities)
 
-            if (influence> max_influence): 
+            if (influence > max_influence):
                 max_influence = influence
                 best_seeds = seeds
-            
-        #print("Best Seeds: [%s] Result: %.2f" % (','.join(str(n) for n in seeds), max_influence))
-        
-        return best_seeds, max_influence
 
-        
+        # print("Best Seeds: [%s] Result: %.2f" % (','.join(str(n) for n in seeds), max_influence))
+
+        return best_seeds, max_influence
