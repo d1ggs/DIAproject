1d12cb259b07a3a97da4af3250c77ddecb85605d
Federica Ilaria Cesti <37806440+FedericaCesti@users.noreply.github.com>
2020-05-08 16:36:30 +0200

    fixed stationary env experiment

--- pricing/stationary_experiment.py
+++ pricing/stationary_experiment.py
@@ -1,61 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 from tqdm import trange
-
-from pricing.environments import EnvironmentUCB
+from pricing.conversion_rate import ProductConversionRate
+from pricing.environments import StationaryEnvironment
 from pricing.learners.UCBLearner import UCBLearner
 from pricing.learners.ts_learner import TSLearner
-pricess = [[4,3,2,1,5,6],[40,30,20,10,50,60],[400,300,200,100,500,600],[4000,3000,2000,1000,5000,6000]]
-#pricess = [[63, 76, 10, 8, 53, 21],[8,10,21,53,63,76],[10,53,76,63,21,8],[76,63,53,21,10,8]]
+
+PRICES = [2,3,4,5,6,7,8,9,10,11]
 T = 100
-arms = [0, 1, 2, 3, 4, 5]
-n_arms = len(arms)
+N_ARMS = 10
+with open("products/products.json", 'r') as productfile:
+    p_info = json.load(productfile)
+    productfile.close()
+
+    p_2 = p_info["products"][1]
+    p_id = p_2["product_id"]
+    seasons = p_2["seasons"]
+    s_id = seasons[0]["season_id"]
+    y = seasons[0]["y_values"]
+    curve = ProductConversionRate(p_id, s_id, N_ARMS, y)
+
 n_experiments = 50
-for prices in pricess:
-    reward_per_experiment_ucb = []
-    reward_per_experiment_ts = []
-    env = EnvironmentUCB(arms=arms, prices=prices)
 
-    regret_per_experiment_ucb = []
-    regret_per_experiment_ts = []
+reward_per_experiment_ucb = []
+reward_per_experiment_ts = []
+env = StationaryEnvironment(prices=PRICES, curve=curve)
 
-    for e in trange(n_experiments):
-        ucb_learner = UCBLearner(n_arms, prices)
-        ts_learner = TSLearner(n_arms, prices)
+regret_per_experiment_ucb = []
+regret_per_experiment_ts = []
 
-        cumulative_regret_ucb = 0
-        cumulative_regret_ts = 0
+for e in trange(n_experiments):
+    ucb_learner = UCBLearner(N_ARMS, PRICES)
+    ts_learner = TSLearner(N_ARMS, PRICES)
 
-        regrets_ucb_per_timestep = []
-        regrets_ts_per_timestep = []
+    cumulative_regret_ucb = 0
+    cumulative_regret_ts = 0
 
-        for t in range(T):
-            clicks = round(np.random.normal(10, 0.1))
-            # clicks = 10
+    regrets_ucb_per_timestep = []
+    regrets_ts_per_timestep = []
 
-            for _ in range(clicks):
-                #UCB
-                pulled_arm = ucb_learner.pull_arm()
-                reward = env.round(pulled_arm)
-                ucb_learner.update(pulled_arm, reward)
+    for t in range(T):
+        clicks = round(np.random.normal(10, 0.1))
+        # clicks = 10
 
-                instantaneous_regret = env.get_inst_regret(pulled_arm)
-                cumulative_regret_ucb += instantaneous_regret
+        for _ in range(clicks):
+            #UCB
+            pulled_arm = ucb_learner.pull_arm()
+            reward = env.round(pulled_arm)
+            ucb_learner.update(pulled_arm, reward)
 
-                #TS
-                pulled_arm = ts_learner.pull_arm()
-                reward = env.round(pulled_arm)
-                ts_learner.update(pulled_arm, reward)
+            instantaneous_regret = env.get_inst_regret(pulled_arm)
+            cumulative_regret_ucb += instantaneous_regret
 
-                instantaneous_regret = env.get_inst_regret(pulled_arm)
-                cumulative_regret_ts += instantaneous_regret
+            #TS
+            pulled_arm = ts_learner.pull_arm()
+            reward = env.round(pulled_arm)
+            ts_learner.update(pulled_arm, reward)
 
-            regrets_ucb_per_timestep.append(cumulative_regret_ucb)
-            regrets_ts_per_timestep.append(cumulative_regret_ts)
+            instantaneous_regret = env.get_inst_regret(pulled_arm)
+            cumulative_regret_ts += instantaneous_regret
+
+        regrets_ucb_per_timestep.append(cumulative_regret_ucb)
+        regrets_ts_per_timestep.append(cumulative_regret_ts)
 
 
-        regret_per_experiment_ts.append(regrets_ts_per_timestep)
-        regret_per_experiment_ucb.append(regrets_ucb_per_timestep)
+    regret_per_experiment_ts.append(regrets_ts_per_timestep)
+    regret_per_experiment_ucb.append(regrets_ucb_per_timestep)
 
             # ucb_regret_per_timestep.append(np.mean(regret_ucb))
             # gts_regret_per_timestep.append(np.mean(regret_gts))
@@ -67,11 +78,11 @@ for prices in pricess:
 
     # print(reward_per_experiment)
     # print(optimal_reward)
-    plt.figure()
-    plt.xlabel("Time")
-    plt.ylabel("Regret")
-    # TODO check if better plotting expected regret or mean regret
-    #plt.plot(np.mean(regret_per_experiment_ucb, axis=0), 'r')
-    plt.plot(np.mean(regret_per_experiment_ts, axis=0), 'b')
-    #plt.legend(['UCB1', "TS"])
-    plt.show()
+plt.figure()
+plt.xlabel("Time")
+plt.ylabel("Regret")
+# TODO check if better plotting expected regret or mean regret
+#plt.plot(np.mean(regret_per_experiment_ucb, axis=0), 'r')
+plt.plot(np.mean(regret_per_experiment_ts, axis=0), 'b')
+#plt.legend(['UCB1', "TS"])
+plt.show()
