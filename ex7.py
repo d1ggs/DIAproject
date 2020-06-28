Fixed LinUCBTest regret computation, plotted regret figures

ex7.py
@@ -18,14 +18,13 @@ from social_influence.social_setup import SocialNetwork
 from social_influence.mc_sampling import MonteCarloSampling
 from social_influence.budget_allocation import StatelessBudgetAllocation
 
-from social_influence.LinUCB.InfluenceMaximizator import InfluenceMaximizator
 from social_influence.LinUCB.LinUCBLearner import LinUCBLearner
 
 # Social influence constants
-MAX_NODES = 30
-TOTAL_BUDGET = 15
-MAX_PROPAGATION_STEPS = 10
-N_EXPERIMENTS = 3
+MAX_NODES = 300
+TOTAL_BUDGET = 5
+MAX_PROPAGATION_STEPS = 5
+N_EXPERIMENTS = 10
 
 SOCIAL_NAMES = ["gplus", "facebook", "wikipedia"]
 # PARAMETERS = np.array(
@@ -178,3 +177,5 @@ if __name__ == "__main__":
         sns.lineplot(x="timestep", y="regret", data=df, hue="agent")
         plt.title(social_network + " - product " + str(product_index + 1) + " : mean regret over time")
         plt.savefig("ex_7_" + social_network + ".png")
+        plt.show()
+

social_influence/LinUCB/LinUCBEnviroment.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from social_influence.influence_maximisation import GreedyLearner
+
 
 class LinUCBEnviroment():
     def __init__(self, probability_matrix):
@@ -13,5 +15,15 @@ class LinUCBEnviroment():
             return 0
 
 
-    def opt(self):
-        return np.max(self.probability_matrix)
+    def opt(self, budget, mc_simulations, n_steps, parallel=False):
+        """
+
+        :return: il seed ottimo
+        """
+        greedy_learner = GreedyLearner(self.probability_matrix, self.probability_matrix.shape[0])
+        if parallel:
+            seed, best_reward = greedy_learner.parallel_fit(budget, mc_simulations, n_steps)
+        else:
+            seed, best_reward = greedy_learner.fit(budget, mc_simulations, n_steps)
+
+        return best_reward - budget, seed